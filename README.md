017cde6dd34ea8b388ce4c91b632de8dd3c29cbc
ike <40803799+ikmnjrd@users.noreply.github.com>
2021-12-31 20:21:28 +0900

    spectrum analyzer

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 
-
 ### ローカルサーバー起動
 ```bash
 $ cd ./docs
 $ python -m SimpleHTTPServer 8080
 ```
+
+### デモ
+[https://ikmnjrd.github.io/spectrum-visualizer](https://ikmnjrd.github.io/spectrum-visualizer)
--- docs/script.js
+++ docs/script.js
@@ -7,8 +7,8 @@ const init = () => {
 
   // canvasを取得
   const canvas = document.getElementById('canvas');
-  canvas.width = 512;
-  canvas.height = 288;
+  canvas.width = window.innerWidth -4;
+  canvas.height = window.innerHeight -4;
 
   // canvasのcontextを取得
   const canvasCtx = canvas.getContext("2d");
@@ -26,81 +26,62 @@ const init = () => {
   const audioEl = document.getElementById('audioController');
 
   upload_file.addEventListener('change', (e) => {
-    console.log(e);
-    console.log(e.target.files[0]);
-
     const audio_blob = e.target.files[0];
 
     audioEl.src = URL.createObjectURL(audio_blob);
 
+    const AudioBufferSourceNode = audioCtx.createMediaElementSource(audioEl);
+    const MediaElementAudioSourceNode = audioCtx.createBufferSource();
+    console.log("createBufferSource:", MediaElementAudioSourceNode);
+    console.log("createMediaElementSource", AudioBufferSourceNode)
 
-    // audioEl.load();
-    // audioEl.play();
+    AudioBufferSourceNode.connect(audioCtx.destination);
+    AudioBufferSourceNode.connect(analyser);
 
-    const fr = new FileReader();
-    fr.onload = function() {
-        // バイナリデータ
-        const arrayBuffer = fr.result;
-        console.log("arrayBuffer: ", arrayBuffer);
-        decode(arrayBuffer);
-    };
-    fr.readAsArrayBuffer(audio_blob);
+    analyser.fftSize = 512;
+    audioEl.load()
+    audioEl.play()
+    MediaElementAudioSourceNode.start(0);
+    draw();
 
-    // draw(audioEl, audio_blob);
   });
 
 
-
-  // decode処理
-  const decode = (arrayBuffer) => {
-    audioCtx.decodeAudioData(arrayBuffer, (audioBuffer) => {
-        // AudioNodeを作成
-        const source = audioCtx.createBufferSource();
-        const source_test = audioCtx.createMediaElementSource(audioEl);
-        console.log(source_test);
-        // bufferプロパティにAudioBufferを指定（????）
-        // source.buffer = audioBuffer;
-        // 音声出力先を指定
-        source_test.connect(audioCtx.destination);
-        // AnalyserNodeを指定
-        source_test.connect(analyser);
-
-        analyser.fftSize = 128;
-
-        audioEl.play()
-
-        // // 再生開始
-        // audioEl.play()
-        console.log(source);
-
-        source.start(0);
-        // audioEl.src = source;
-        // audioEl.load();
-        // audioEl.play();
-        draw();
-    });
-  };
-
   const draw = () => {
     const dataArray = new Uint8Array(analyser.fftSize);
-    console.log(dataArray);
     // dataArrayが解析されたデータで満たされる
     analyser.getByteTimeDomainData(dataArray);
-    console.log(analyser)
+    console.log("analyser", analyser)
+    console.log("dataArray", dataArray);
 
-    const sliceWidth = canvas.width / analyser.fftSize;
+    /* spectrums */
+    const spectrums = new Uint8Array(analyser.frequencyBinCount);  // Array size is 1024 (half of FFT size)
+    analyser.getByteFrequencyData(spectrums);
+    console.log("spectrums", spectrums)
+
+    // const sliceWidth = canvas.width / analyser.fftSize;
+    const sliceWidth = canvas.width / analyser.frequencyBinCount;
 
     canvasCtx.beginPath();
-    // canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
-    canvasCtx.moveTo(0, canvas.height / 2);
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+    // canvasCtx.moveTo(0, canvas.height / 2);
 
-    for (let i = 0; i < analyser.fftSize; i++) {
-        var x = sliceWidth * i;
-        // dataArrayの中身は0から255の数値
-        var v = dataArray[i] / 128;
-        var y = v * canvas.height / 2;
-        canvasCtx.lineTo(x, y);
-    }
+    /* 元のやつ */
+    // for (let i = 0; i < analyser.fftSize; i++) {
+    //     var x = sliceWidth * i;
+    //     // dataArrayの中身は0から255の数値
+    //     var v = dataArray[i] / 128;
+    //     var y = v * canvas.height / 2;
+    //     canvasCtx.lineTo(x, y);
+    // }
+
+    /* spectrums */
+    const len = spectrums.length;
+    for (let i = 0; i < len; i++) {
+      const x = i / len;
+      const y = 1 - (spectrums[i] / 255);
+      canvasCtx.lineTo(x * canvas.width, y * canvas.height);
+  }
 
     canvasCtx.lineTo(canvas.width, canvas.height / 2);
     canvasCtx.stroke();
