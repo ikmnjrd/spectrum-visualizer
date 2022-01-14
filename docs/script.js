96f423a208adc6ca3e6e0fee8f0b65842af11308
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-01-14 18:57:36 +0900

    fix

--- docs/script.js
+++ docs/script.js
@@ -16,7 +16,7 @@ const init = () => {
 
   // canvasのcontextを取得
   const canvasCtx = canvas.getContext("2d");
-  canvasCtx.fillStyle = 'rgb(16, 16, 24)';
+  // canvasCtx.fillStyle = 'rgb(16, 16, 24)';
   canvasCtx.fillStyle = 'rgb(255, 255, 255)';
   canvasCtx.lineWidth = 2;
   canvasCtx.strokeStyle = 'rgb(124, 224, 255)';
@@ -37,25 +37,21 @@ const init = () => {
   // X軸描画
   // canvasCtx.textAlign = "center";
   // canvasCtx.textBaseline = "top";
-  canvasCtx.strokeStyle = "rgb(0, 0, 0)";
+  // canvasCtx.strokeStyle = "rgb(0, 0, 0)";
   canvasCtx.moveTo( groundX0, groundY0 );
   canvasCtx.lineTo( groundX0+groundW-20, groundY0 );
   // 10の部分はpitchXが入っていた
   for (let i = 0; i < groundW / pichX; i++){
     canvasCtx.moveTo( groundX0 + i * pichX, groundY0 );
-    canvasCtx.lineTo( groundX0 + i * pichX, groundY0 - 5 );
+    canvasCtx.lineTo( groundX0 + i * pichX, groundY0 + 10 );
     canvasCtx.fillText(i, groundX0 + i * pichX, groundY0 + 2 );
   }
 
 
 
-
-
   canvasCtx.stroke();
-  canvasCtx.restore();                    //描画条件を元に戻す
-  canvasCtx.save();                       //現在の描画条件を保管
-  canvasCtx.beginPath();                  //現在のパスをリセット
-
+  // canvasCtx.restore();                    //描画条件を元に戻す
+  // canvasCtx.save();                       //現在の描画条件を保管
 
   // 初起表示
   // canvasCtx.fillRect(spcW, spcH, groundW, groundH);
@@ -66,6 +62,7 @@ const init = () => {
 
   const upload_file = document.getElementById('uploadedFile');
   const audioEl = document.getElementById('audioController');
+  audioEl.volume = 0.1;
 
   upload_file.addEventListener('change', (e) => {
     const audio_blob = e.target.files[0];
@@ -88,57 +85,47 @@ const init = () => {
 
   });
 
+  console.log(groundY0)
 
   const draw = () => {
     const dataArray = new Uint8Array(analyser.fftSize);
     // dataArrayが解析されたデータで満たされる
     analyser.getByteTimeDomainData(dataArray);
-    console.log("analyser", analyser)
-    console.log("dataArray", dataArray);
+    // console.log("analyser", analyser)
+    // console.log("dataArray", dataArray);
 
     /* spectrums */
     const spectrums = new Uint8Array(analyser.frequencyBinCount);  // Array size is 1024 (half of FFT size)
     analyser.getByteFrequencyData(spectrums);
-    console.log("spectrums", spectrums)
+    // console.log("spectrums", spectrums)
 
     // const sliceWidth = canvas.width / analyser.fftSize;
     const sliceWidth = canvas.width / analyser.frequencyBinCount;
 
     canvasCtx.beginPath();
-    canvasCtx.fillRect(spcW, spcH, groundW, groundH);
-    // canvasCtx.moveTo(0, canvas.height / 2);
+    // canvasCtx.fillRect(spcW, spcH, groundW, groundH);
+    // canvasCtx.moveTo(spcW, groundY0 + (groundH / 2));
 
-    /* 元のやつ */
-    // for (let i = 0; i < analyser.fftSize; i++) {
-    //     var x = sliceWidth * i;
-    //     // dataArrayの中身は0から255の数値
-    //     var v = dataArray[i] / 128;
-    //     var y = v * canvas.height / 2;
-    //     canvasCtx.lineTo(x, y);
-    // }
 
     /* spectrums */
     const len = spectrums.length;
+    console.log("len: ", len);
     for (let i = 0; i < len; i++) {
       const x = i / len;
       const y = 1 - (spectrums[i] / 255);
-      canvasCtx.lineTo(x * canvas.width + spcW, y * canvas.height + spcH);
+      canvasCtx.lineTo(x * groundW + groundX0, y * groundH );
   }
 
-    canvasCtx.lineTo(groundW, groundH / 2);
-    canvasCtx.stroke();
+  // canvasCtx.lineTo(canvas.width, groundY0 + (groundH / 2));
+  canvasCtx.fillRect(spcW, spcH, groundW - spcW, groundH -spcH);
 
-    canvasCtx.beginPath();
+    canvasCtx.stroke();
 
 
     // 毎アニメーションでdrawを呼ぶ
     requestAnimationFrame(draw);
   }
 
-
-  const drawLabel =()=> {
-
-  }
 }
 
 
