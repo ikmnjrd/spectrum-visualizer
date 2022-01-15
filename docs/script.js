a0ce25334962e67c8ef7ee889b6811e756ef4894
ike <40803799+ikmnjrd@users.noreply.github.com>
2022-01-15 23:49:29 +0900

    X軸目盛り対応

--- docs/script.js
+++ docs/script.js
@@ -11,8 +11,8 @@ const init = () => {
   // canvas.height = window.innerHeight -4;
 
   // 適当に、ウィンドウサイズよりやや小さめに
-  canvas.width = window.innerWidth * 0.6;
-  canvas.height = window.innerHeight * 0.6;
+  canvas.width = window.innerWidth * 0.8;
+  canvas.height = window.innerHeight * 0.8;
 
   // canvasのcontextを取得
   const canvasCtx = canvas.getContext("2d");
@@ -21,37 +21,57 @@ const init = () => {
   canvasCtx.lineWidth = 2;
   canvasCtx.strokeStyle = 'rgb(124, 224, 255)';
 
+  canvasCtx.strokeStyle = 'rgb(124, 224, 255)';
+
   // 目盛りの設定
   const spcW = 40;            // Y軸左のスペース
-  const spcH = 30;            // X軸下のスペース
+  const spcH = 50;            // X軸下のスペース
   const groundW = canvas.width - spcW; // グラフ領域W
   const groundH = canvas.height - spcH;// グラフ領域H
   const groundX0 = spcW;
   const groundY0= groundH;   // 原点位置
   // const pichX = groundW / datas.length;            // X目盛ピッチ
-  const pichX = 20; //対数表示したい
+  // const pichX = 20; //対数表示したい
   // const DmYMx = Math.pow(10, Math.ceil(Math.log(yMax) / Math.log(10)));
   // const cnstH= 210 / DmYMx;
   // const pichH = DmYMx / 4;//Y目盛ピッチ
 
+  analyser.fftSize = 512;
+  console.log("Sample Rate:", audioCtx.sampleRate);
+  console.log("fftSize:", analyser.fftSize);
+  // 周波数分解能を求める
+  const freqReso = audioCtx.sampleRate / analyser.fftSize
+  console.log("Frequency Resolution:", freqReso);
+  // const pichX = audioCtx.sampleRate ;
+  const pichX = freqReso;
+  // console.log("pichX:", pichX);
+
   // X軸描画
-  // canvasCtx.textAlign = "center";
-  // canvasCtx.textBaseline = "top";
+  canvasCtx.textAlign = "left";
+  canvasCtx.textBaseline = "middle";
+  canvasCtx.font = "6pt Arial";
+  canvasCtx.fillStyle = 'rgb(0, 0, 0)';
+  canvasCtx
   // canvasCtx.strokeStyle = "rgb(0, 0, 0)";
   canvasCtx.moveTo( groundX0, groundY0 );
   canvasCtx.lineTo( groundX0+groundW-20, groundY0 );
-  // 10の部分はpitchXが入っていた
-  for (let i = 0; i < groundW / pichX; i++){
-    canvasCtx.moveTo( groundX0 + i * pichX, groundY0 );
-    canvasCtx.lineTo( groundX0 + i * pichX, groundY0 + 10 );
-    canvasCtx.fillText(i, groundX0 + i * pichX, groundY0 + 2 );
+
+  for (let i = 0; i < freqReso; i++){
+  // for (let i = 0; i <freqReso ; i++){
+    canvasCtx.moveTo( groundX0 + groundW * i /freqReso , groundY0 );
+    canvasCtx.lineTo( groundX0 +  groundW * i/ freqReso, groundY0 + 10 );
+    canvasCtx.save()
+    //回転の中心を移動
+    canvasCtx.translate(groundX0+  groundW * i / freqReso , groundY0 +10);
+    canvasCtx.rotate( 90 * Math.PI / 180);
+    canvasCtx.fillText( i* freqReso, 0, 0);
+    canvasCtx.restore();
+    // canvasCtx.fillText( "ffff", groundX0 + i * pichX, 300 );
+
   }
 
-
-
   canvasCtx.stroke();
-  // canvasCtx.restore();                    //描画条件を元に戻す
-  // canvasCtx.save();                       //現在の描画条件を保管
+  canvasCtx.fillStyle = 'rgb(255, 255, 255)';
 
   // 初起表示
   // canvasCtx.fillRect(spcW, spcH, groundW, groundH);
@@ -62,8 +82,10 @@ const init = () => {
 
   const upload_file = document.getElementById('uploadedFile');
   const audioEl = document.getElementById('audioController');
-  audioEl.volume = 0.1;
+  audioEl.volume = 0.05;
 
+  //
+  //
   upload_file.addEventListener('change', (e) => {
     const audio_blob = e.target.files[0];
 
@@ -77,7 +99,6 @@ const init = () => {
     AudioBufferSourceNode.connect(audioCtx.destination);
     AudioBufferSourceNode.connect(analyser);
 
-    analyser.fftSize = 512;
     audioEl.load()
     audioEl.play()
     MediaElementAudioSourceNode.start(0);
@@ -85,7 +106,6 @@ const init = () => {
 
   });
 
-  console.log(groundY0)
 
   const draw = () => {
     const dataArray = new Uint8Array(analyser.fftSize);
@@ -109,15 +129,14 @@ const init = () => {
 
     /* spectrums */
     const len = spectrums.length;
-    console.log("len: ", len);
+    canvasCtx.moveTo(groundX0, groundY0)
     for (let i = 0; i < len; i++) {
-      const x = i / len;
       const y = 1 - (spectrums[i] / 255);
-      canvasCtx.lineTo(x * groundW + groundX0, y * groundH );
-  }
+      canvasCtx.lineTo((i / len)  * groundW + groundX0, y * groundH );
+    }
 
-  // canvasCtx.lineTo(canvas.width, groundY0 + (groundH / 2));
-  canvasCtx.fillRect(spcW, spcH, groundW - spcW, groundH -spcH);
+    // canvasCtx.lineTo(canvas.width, groundY0 + (groundH / 2));
+    canvasCtx.fillRect(spcW, spcH, groundW - spcW, groundH -spcH);
 
     canvasCtx.stroke();
 
